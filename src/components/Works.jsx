71b10283dabf759c118dacec21c37a0f7cea7e6f
Nikhil Jatale <nikhiljatale@gmail.com>
2023-03-11 22:11:24 +0000

    update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -80,11 +80,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Through a variety of projects, I have demonstrated my skills and experience as a data analyst. Each project in my portfolio includes a brief description and links to relevant code and demos, illustrating my ability to solve complex problems and work with diverse technologies.
         </motion.p>
       </div>
 
